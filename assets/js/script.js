5f4c722fb8c0158e905f7da0c5541ef33bb2753d
Chr15w1986 <chr15w1986@hotmail.co.uk>
2021-11-25 18:59:17 +0000

    create countdown function of timer

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+// lowest possible DOM handling for milliseconds is 4ms.
+
+let throttleAmount = 4;
+
+countdown(10);
+
+function countdown(secs) {
+    let milli = secs * (1000);
+    let counter = setInterval(function() {
+        if(milli <= 0) {
+            clearInterval(counter);
+            return
+        }
+        milli -= throttleAmount;
+        document.getElementById("milliseconds").innerHTML = `${milli} ms`;
+    }, throttleAmount);
+}
+
+// function button {
+//     document.getElementById("btn").onclick = 
+// }
