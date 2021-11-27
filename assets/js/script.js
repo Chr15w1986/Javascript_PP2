fde4081b5b57e6a579645cf1572451928dc8e8cf
Chr15w1986 <chr15w1986@hotmail.co.uk>
2021-11-27 08:39:29 +0000

    edited countdown function code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 
 // lowest possible DOM handling for milliseconds is 4ms.
-
+// Countdown timer functions:
 let throttleAmount = 4;
 
 countdown(10);
 
 function countdown(secs) {
-    let milli = secs * (1000);
+    let milli = secs * 1000;
     let counter = setInterval(function() {
         if(milli <= 0) {
             clearInterval(counter);
@@ -16,7 +16,3 @@ function countdown(secs) {
         document.getElementById("milliseconds").innerHTML = `${milli} ms`;
     }, throttleAmount);
 }
-
-// function button {
-//     document.getElementById("btn").onclick = 
-// }
