6fa3a56f72560da3508a9deff8c49023902bfef0
Chr15w1986 <chr15w1986@hotmail.co.uk>
2021-11-30 19:57:36 +0000

    reworked stop and reset code to ensure correct function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,35 @@
 // lowest possible DOM handling for milliseconds is 4ms.
+
 // Countdown timer functions:
 const throttleAmount = 4;
 
-countdown(5);
-
 function countdown(secs) {
     let timer = secs * 1000;
-    let counter = setInterval(function() {
-        if(timer <= 0) {
+    let counter = setInterval(function () {
+        if (timer <= 0) {
             clearInterval(counter);
             return
         }
-        
+
+// Stack over flow fix to get millisecond timer to work correctly.
         timer -= throttleAmount;
-        document.getElementById("milliseconds").innerHTML = `${timer} ms`;
+        document.getElementById("milliseconds").innerHTML = `${timer}ms`;
     }, throttleAmount);
 }
 
 // Stop and Reset button functions:
-document.getElementById("btn").addEventListener("click", function(){
-    this.querySelector("#btn").click(function(){
-        clearInterval(counter);
-    });
+document.getElementById("btn").addEventListener("click", function () {
+    clearInterval(countdown(0));
+    alert("Oooh So Close!");
 });
 
-document.getElementById("btn2").addEventListener("click", function(){
-    this.querySelector("#btn2").click(function(){
-        setInterval(counter);
-        alert("Clock reset, Try Again");
-    });
+document.getElementById("btn2").addEventListener("click", function () {
+    setInterval(countdown(5));
+    if (counter <= 0) {
+        setInterval(counter(5));
+            return
+    }
+    alert("Clock reset, Try Again");
 });
+
+countdown(5);
