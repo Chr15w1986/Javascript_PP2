5c406f9ed9e5a7e1e6508f9023550a1585036de9
Chr15w1986 <chr15w1986@hotmail.co.uk>
2021-12-07 21:05:48 +0000

    reworked start timer function added extra opacity setting to become more transparent when timer gets closer to zero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,24 @@
-// lowest possible DOM handling for milliseconds is 4ms.
-
 // Countdown timer functions:
 let counter = null;
 const timer = 5000;
-// Stack over flow fix to get milliseconds to work correctly.
+
+// Stack over flow fix to get milliseconds to work correctly, lowest possible DOM handling for milliseconds is 4ms.
 const throttleAmount = 4;
 
-function countdown(secs) {
-    timer = secs; //timer value for when the function is called, is being set by the number or timer value that is being passed in.
+document.getElementById("milliseconds").innerHTML = `${timer}ms`;
+
+function startCountdown(timer) {
+    let secs = timer;
     counter = setInterval(function () {
-        if (timer <= 4) { //if timer is less than or equal to 4, do this and then continue to line 20.
-            clearInterval(counter); //stops the timer
-        }
 
-        // Stack over flow fix to get milliseconds to work correctly.
-        timer -= throttleAmount;
-        document.getElementById("milliseconds").innerHTML = `${timer}ms`;
-    }, throttleAmount); //do this every 4 milliseconds
+        // this reduces the timer by throttle amount of 4ms
+        secs -= throttleAmount;
+        document.getElementById("milliseconds").innerHTML = `${secs}ms`;
+        // Reduces the opacity of the timer the closer it gets to zero
+        document.getElementById("milliseconds").style.color = `rgba(32, 30, 30, ${(secs/timer)}`;
+
+    }, throttleAmount);
 }
-
-// Start, Stop and Reset buttons:
-// Start timer:
-document.getElementById("start").addEventListener("click", function () {
-    countdown(timer); //this runs countdown with the current value of timer
-});
 
 // Stop timer:
 document.getElementById("stop").addEventListener("click", function () {
